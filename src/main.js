a0d8762fe10fb709d225a9a238d5380d33be7c35
rblinde <robertblinde@gmail.com>
2020-01-05 15:11:57 +0100

    :sparkles: Added core functionality

---
+++ src/main.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const FileSaver = require('file-saver');
+const data = require('./data.json');
+
+/**
+ * Creates datetime string
+ * @param  {String} date date from csv
+ * @param  {String} time time from csv
+ * @return {String}      date string (DMYTHm00)
+ */
+function formatDate(date, time) {
+  const [day, month, year] = date.split('/');
+  const [hour, minute] = time.split(':');
+  return `${year}${month}${day}T${hour}${minute}00`;
+}
+
+/**
+ * Creates string with calendar in ICS format
+ * @param  {Collection} events
+ * @return {String}            ICS string
+ */
+function createSchedule(events) {
+  const header = 'BEGIN:VCALENDAR\nVERSION:2.0\nMETHOD:PUBLISH\nPRODID:-//rblinde/schedule-builder v1.0//EN';
+  const footer = '\nEND:VCALENDAR';
+
+  let body = '';
+
+  for (const event of events) {
+    const { start, end, date, course, label, location } = event;
+    const eventStart = formatDate(date, start);
+    const eventEnd = formatDate(date, end);
+    const eventString = '\nBEGIN:VEVENT' +
+    `\nUID:${eventEnd}-${course}@rblinde/schedule-builder` +
+    `\nSUMMARY:${label}` +
+    `\nDTSTART;TZID=Europe/Amsterdam:${eventStart}` +
+    `\nDTEND;TZID=Europe/Amsterdam:${eventEnd}` +
+    `\nLOCATION:${location}` +
+    '\nSEQUENCE:0' +
+    '\nEND:VEVENT';
+
+    body += eventString;
+  }
+
+  return header + body + footer;
+}
+
+/**
+ * Checks which courses have been picked, generates
+ * ICS file and downloads it to device
+ */
+function handleBuildBtnClick() {
+  const checkboxes = [...document.getElementsByName('courses')];
+  const selectedCourseIds = checkboxes
+    .filter(ch => !!ch.checked)
+    .map(el => el.value);
+
+  if (selectedCourseIds.length === 0) {
+    return false;
+  }
+
+  const events = data.filter(row => selectedCourseIds.includes(row.course));
+  const schedule = createSchedule(events);
+  // Downloading
+  const blob = new Blob([schedule], { type: 'text/plain;charset=utf-8' });
+  FileSaver.saveAs(blob, `calendar.ics`);
+}
+
+const buildBtn = document.getElementById('build-btn');
+buildBtn.addEventListener('click', handleBuildBtnClick);
